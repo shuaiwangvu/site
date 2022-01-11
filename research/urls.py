72492a106958c71c5eab074e3e67a9af6eadf3de
shuaiwangvu <shuai.wang.vu@gmail.com>
2022-01-11 13:01:32 +0100

    update data page

--- research/urls.py
+++ research/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blog/', views.blog, name='blog'),
     path('publication/', views.publication, name='publication'),
-    path('tools', views.tools, name='tools'),
-    # path('resume', views.resume, name='resume'),
-    path('teaching', views.teaching, name='teaching'),
-    path('activities', views.activities, name='activities'),
     path('data', views.data, name='data'),
+    path('tools', views.tools, name='tools'),
+    path('blog', views.resume, name='blog'),
+    path('teaching', views.teaching, name='teaching'),
+    path('activities', views.activities, name='activities')
 ]
