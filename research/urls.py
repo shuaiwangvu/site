8966cb9aed667d29e6d91eeb551590cd40a66cde
shuaiwangvu <shuai.wang.vu@gmail.com>
2022-05-31 13:35:04 +0200

    update website

--- research/urls.py
+++ research/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('publication/', views.publication, name='publication'),
     path('data', views.data, name='data'),
     path('tools', views.tools, name='tools'),
-    path('blog/', views.blog, name='blog'),
+    path('supervision/', views.supervision, name='supervision'),
     path('teaching', views.teaching, name='teaching'),
     path('activities', views.activities, name='activities')
 ]
